32f0c19f462e15bd17c8881a154ee3ed7433c995
Tkacz <gabrielmt2@al.insper.edu.br>
2021-03-07 20:37:36 -0300

    bubblesort func done

--- bubblesort.py
+++ bubblesort.py
@@ -1,1 +1,13 @@
-import _tkinter
+import time
+
+def bubblesort(data, draw, timer):
+    n=len(data)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if data[j]>data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
+                drawData(data, ['g' if x == j + 1 else 'r' for x in range(len(data))]) 
+                time.sleep(timer) 
+          
+    # sorted elements geneated with g color 
+    drawData(data, ['g' for x in range(len(data))]) 
